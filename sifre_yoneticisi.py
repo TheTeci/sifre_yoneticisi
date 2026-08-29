043febb04a0fb011fb0eb5b5428763f4fb33c99a
TheTeci <ateci.0506@gmail.com>
2026-08-30 02:29:41 +0300

    şifreleme anahtarı oluşturma eklendi

--- sifre_yoneticisi.py
+++ sifre_yoneticisi.py
@@ -1,4 +1,16 @@
 import sqlite3 as sql
+from cryptography.fernet import Fernet
+import os
+
+if  os.path.exists("anahtar.key"):
+    with open("anahtar.key", "rb") as key_file:
+        key = key_file.read()
+else:
+    key = Fernet.generate_key()
+    with open("anahtar.key", "wb") as key_file:
+        key_file.write(key)
+
+fernet = Fernet(key)
 
 conn = sql.connect('sifre_yoneticisi.db')
 
