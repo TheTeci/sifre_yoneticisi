625692a57e9cdb5d1d9187ee403f54f828be6424
TheTeci <ateci.0506@gmail.com>
2026-08-30 01:51:49 +0300

    Tablo oluşturuldu

--- sifre_yoneticisi.py
+++ sifre_yoneticisi.py
@@ -3,6 +3,11 @@ import sqlite3 as sql
 conn = sql.connect('sifre_yoneticisi.db')
 
 cursor = conn.cursor()
-
+cursor.execute('''CREATE TABLE IF NOT EXISTS sifreler (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    site_adi TEXT NOT NULL,
+    kullanici_adi TEXT NOT NULL,
+    sifre TEXT NOT NULL
+)''')
 conn.commit()
 conn.close()
